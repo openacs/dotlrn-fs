b7237e787d57695a5d7229ac816a225d22cbb13d
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2014-08-18 21:50:27 +0000

    - modernize SQL (dollar quoting)

--- sql/postgresql/dotlrn-fs-create.sql
+++ sql/postgresql/dotlrn-fs-create.sql
@@ -27,143 +27,150 @@
 -- ported to postgres 2002-07-09
 
 
-create function inline_0()
-returns integer as '
-declare
+
+
+--
+-- procedure inline_0/0
+--
+CREATE OR REPLACE FUNCTION inline_0(
+
+) RETURNS integer AS $$
+DECLARE
 	foo integer;
-begin
+BEGIN
 	-- create the implementation
 	foo := acs_sc_impl__new (
-		''dotlrn_applet'',
-		''dotlrn_fs'',
-		''dotlrn_fs''
+		'dotlrn_applet',
+		'dotlrn_fs',
+		'dotlrn_fs'
 	);
 
 	-- add all the hooks
 
 	-- GetPrettyName
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''GetPrettyName'',
-	       ''dotlrn_fs::get_pretty_name'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'GetPrettyName',
+	       'dotlrn_fs::get_pretty_name',
+	       'TCL'
 	);
 
 	-- AddApplet
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''AddApplet'',
-	       ''dotlrn_fs::add_applet'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'AddApplet',
+	       'dotlrn_fs::add_applet',
+	       'TCL'
 	);
 
 	-- RemoveApplet
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''RemoveApplet'',
-	       ''dotlrn_fs::remove_applet'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'RemoveApplet',
+	       'dotlrn_fs::remove_applet',
+	       'TCL'
 	);
 
 	-- AddAppletToCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''AddAppletToCommunity'',
-	       ''dotlrn_fs::add_applet_to_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'AddAppletToCommunity',
+	       'dotlrn_fs::add_applet_to_community',
+	       'TCL'
 	);
 
 	-- RemoveAppletFromCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''RemoveAppletFromCommunity'',
-	       ''dotlrn_fs::remove_applet_from_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'RemoveAppletFromCommunity',
+	       'dotlrn_fs::remove_applet_from_community',
+	       'TCL'
 	);
 
 	-- AddUser
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''AddUser'',
-	       ''dotlrn_fs::add_user'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'AddUser',
+	       'dotlrn_fs::add_user',
+	       'TCL'
 	);
 
 	-- RemoveUser
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''RemoveUser'',
-	       ''dotlrn_fs::remove_user'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'RemoveUser',
+	       'dotlrn_fs::remove_user',
+	       'TCL'
 	);
 
 	-- AddUserToCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''AddUserToCommunity'',
-	       ''dotlrn_fs::add_user_to_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'AddUserToCommunity',
+	       'dotlrn_fs::add_user_to_community',
+	       'TCL'
 	);
 
 	-- RemoveUserFromCommunity
 	foo := acs_sc_impl_alias__new (
-	       ''dotlrn_applet'',
-	       ''dotlrn_fs'',
-	       ''RemoveUserFromCommunity'',
-	       ''dotlrn_fs::remove_user_from_community'',
-	       ''TCL''
+	       'dotlrn_applet',
+	       'dotlrn_fs',
+	       'RemoveUserFromCommunity',
+	       'dotlrn_fs::remove_user_from_community',
+	       'TCL'
 	);
 
     -- AddPortlet
     foo := acs_sc_impl_alias__new (
-          ''dotlrn_applet'',
-          ''dotlrn_fs'',
-          ''AddPortlet'',
-          ''dotlrn_fs::add_portlet'',
-          ''TCL''
+          'dotlrn_applet',
+          'dotlrn_fs',
+          'AddPortlet',
+          'dotlrn_fs::add_portlet',
+          'TCL'
     );
 
     -- RemovePortlet
     foo := acs_sc_impl_alias__new (
-          ''dotlrn_applet'',
-          ''dotlrn_fs'',
-          ''RemovePortlet'',
-          ''dotlrn_fs::remove_portlet'',
-          ''TCL''
+          'dotlrn_applet',
+          'dotlrn_fs',
+          'RemovePortlet',
+          'dotlrn_fs::remove_portlet',
+          'TCL'
     );
 
     foo := acs_sc_impl_alias__new (
-          ''dotlrn_applet'',
-          ''dotlrn_fs'',
-          ''Clone'',
-          ''dotlrn_fs::clone'',
-          ''TCL''
+          'dotlrn_applet',
+          'dotlrn_fs',
+          'Clone',
+          'dotlrn_fs::clone',
+          'TCL'
     );
 
     foo := acs_sc_impl_alias__new (
-          ''dotlrn_applet'',
-          ''dotlrn_fs'',
-          ''ChangeEventHandler'',
-          ''dotlrn_fs::change_event_handler'',
-          ''TCL''
+          'dotlrn_applet',
+          'dotlrn_fs',
+          'ChangeEventHandler',
+          'dotlrn_fs::change_event_handler',
+          'TCL'
     );
 
     perform acs_sc_binding__new (
-            ''dotlrn_applet'',
-            ''dotlrn_fs''
+            'dotlrn_applet',
+            'dotlrn_fs'
     );
 
     return 0;
 
-end;' language 'plpgsql';
+END;
+$$ LANGUAGE plpgsql;
 
 select inline_0();
 drop function inline_0();
