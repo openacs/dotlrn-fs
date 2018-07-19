14b702b25a5c0cf288ac8458ae48a880b89fd39b
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2018-07-19 12:21:05 +0000

    improve testcases by removing useless expr commands

--- tcl/test/dotlrn-fs-procs.tcl
+++ tcl/test/dotlrn-fs-procs.tcl
@@ -68,17 +68,17 @@ aa_register_case -cats {api smoke db} dotlrn_fs_user_folders {
                        root_err_msg
                 aa_log $root_err_msg
                          
-                aa_true "User root folder OK" [expr {
+                aa_true "User root folder OK" {
                              $check_user_root_folder_id \
-                                 == $user_root_folder_id}]
+                                 == $user_root_folder_id}
                 catch {set check_user_shared_folder_id \
                            [dotlrn_fs::get_user_shared_folder \
                            -user_id $creation_info(user_id)]} shared_err_msg
                     
                 aa_log $shared_err_msg
-                aa_true "User shared folder OK" [expr {
+                aa_true "User shared folder OK" {
                              $check_user_shared_folder_id \
-                                 == $user_shared_folder_id}]                
+                                 == $user_shared_folder_id}                
         } 
 }
 
