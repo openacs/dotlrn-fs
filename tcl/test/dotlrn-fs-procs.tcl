1a1a46504451876909955d1afd81f6446bd3904b
Hector Romojaro <hector.romojaro@gmail.com>
2019-09-10 09:20:40 +0000

    Add new simple tests for dotlrn-* packages

--- tcl/test/dotlrn-fs-procs.tcl
+++ tcl/test/dotlrn-fs-procs.tcl
@@ -82,6 +82,25 @@ aa_register_case -cats {api smoke db} dotlrn_fs_user_folders {
         }
 }
 
+aa_register_case \
+    -cats {api smoke production_safe} \
+    -procs {
+        dotlrn_fs::package_key
+        dotlrn_fs::my_package_key
+        dotlrn_fs::applet_key
+    } \
+    dotlrn_fs__keys {
+
+        Simple test for the various dotlrn_fs::..._key procs.
+
+        @author Héctor Romojaro <hector.romojaro@gmail.com>
+        @creation-date 2019-09-10
+} {
+    aa_equals "Package key" "[dotlrn_fs::package_key]" "file-storage"
+    aa_equals "My Package key" "[dotlrn_fs::my_package_key]" "dotlrn-fs"
+    aa_equals "Applet key" "[dotlrn_fs::applet_key]" "dotlrn_fs"
+}
+
 # Local variables:
 #    mode: tcl
 #    tcl-indent-level: 4
